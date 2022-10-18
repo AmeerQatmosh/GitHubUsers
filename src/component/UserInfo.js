f39167bb2bba6a6e74f5ab7f76ba1e9f48c51718
AmeerQatmosh <ameerqsh@gmail.com>
2022-10-18 11:30:13 +0300

    first commit

---
+++ src/component/UserInfo.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Button,TextInput,StyleSheet,Pressable,View,Text} from 'react-native';
+export default class UserInfo extends React.Component {
+    
+    
+    render(){
+        return ()
+    }
+}
---
+++ src/component/UsersList.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Button,TextInput,StyleSheet,Pressable,View,Text} from 'react-native';
+export default class UsersList extends React.Component {
+    
+    
+    render(){
+        return ()
+    }
+}
